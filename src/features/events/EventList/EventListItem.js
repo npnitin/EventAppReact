a14361fd529a43ef626dfb17ab7f96822c0d364b
Nitin Ashok Pawar <npawar@altimetrik.com>
2019-08-20 18:17:20 +0530

    adds delete event functionality

--- src/features/events/EventList/EventListItem.js
+++ src/features/events/EventList/EventListItem.js
@@ -3,7 +3,7 @@ import { Button ,Segment,Item,Icon,List} from 'semantic-ui-react'
 import EventListAttendee from './EventListAttendee';
 class EventListItem extends Component {
     render() {
-       const {event,selectEvent} = this.props;
+       const {event,selectEvent,deleteEvent} = this.props;
         return (
                   <Segment.Group>
                         <Segment>
@@ -34,7 +34,9 @@ class EventListItem extends Component {
                         </Segment>
                         <Segment clearing>
                           <span>{event.description}</span>
+                          <Button color='red' onClick={()=>deleteEvent(event.id)} as="a" floated="right" content="Delete" />
                           <Button onClick={()=>selectEvent(event)} as="a" color="teal" floated="right" content="View" />
+                         
                         </Segment>
                       </Segment.Group>
         )
--- src/features/events/EventsDashboard/Eventsdashboard.js
+++ src/features/events/EventsDashboard/Eventsdashboard.js
@@ -151,12 +151,17 @@ class Eventsdashboard extends Component {
       selectEvent:null,
     })
   }
+  handleDeleteEvent=(id)=>{
+    this.setState({
+      events:this.state.events.filter(e=>e.id !==id)
+    })
+  }
     render() {
       const{ events,isOpen,selectedEvent }=this.state;
         return (
             <Grid>
                 <Grid.Column width={10}>
-                    <EventList events={events} selectEvent={this.handleSelectEvent} />
+                    <EventList events={events} selectEvent={this.handleSelectEvent} deleteEvent={this.handleDeleteEvent}/>
                     </Grid.Column>
                 <Grid.Column width={6}>
                     <Button positive content='Create Event' onClick={this.handleCreateFormOpen}/>
