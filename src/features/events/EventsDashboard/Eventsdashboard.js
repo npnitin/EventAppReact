ce9e1d0bfd485a530a72e977bd2be0b7b521061b
Nitin Ashok Pawar <npawar@altimetrik.com>
2019-08-20 13:14:05 +0530

    adds static code

---
+++ src/features/events/EventsDashboard/Eventsdashboard.js
@@ -0,0 +1,126 @@
+import React, { Component } from 'react'
+import { Grid, Button } from 'semantic-ui-react';
+import EventList from '../EventList/EventList';
+import EventForm from '../EventForm/EventForm';
+
+const eventsFromDashboard = [
+  {
+    id: '1',
+    title: 'Trip to Tower of London',
+    date: '2018-03-27T11:00:00+00:00',
+    category: 'culture',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
+    city: 'London, UK',
+    venue: "Tower of London, St Katharine's & Wapping, London",
+    hostedBy: 'Bob',
+    hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
+    attendees: [
+      {
+        id: 'a',
+        name: 'Bob',
+        photoURL: 'https://randomuser.me/api/portraits/men/41.jpg'
+      },
+      {
+        id: 'b',
+        name: 'Tom',
+        photoURL: 'https://randomuser.me/api/portraits/men/42.jpg'
+      }
+    ]
+  },
+  {
+    id: '3',
+    title: 'Trip to Pnchgani',
+    date: '2018-03-28T14:00:00+00:00',
+    category: 'drinks',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
+    city: 'London, UK',
+    venue: 'Punch & Judy, Henrietta Street, London, UK',
+    hostedBy: 'Sam',
+    hostPhotoURL: 'https://randomuser.me/api/portraits/women/1.jpg',
+    attendees: [
+      {
+        id: 'b',
+        name: 'Tom',
+        photoURL: 'https://randomuser.me/api/portraits/women/42.jpg'
+      },
+      {
+        id: 'a',
+        name: 'Bob',
+        photoURL: 'https://randomuser.me/api/portraits/women/43.jpg'
+      },
+      {
+        id: 'd',
+        name: 'Carlet',
+        photoURL: 'https://randomuser.me/api/portraits/women/12.jpg'
+      },
+      {
+        id: 'c',
+        name: 'Jenny',
+        photoURL: 'https://randomuser.me/api/portraits/women/4.jpg'
+      }
+    ]
+  },
+  {
+    id: '2',
+    title: 'Trip to Punch and Judy Pub',
+    date: '2018-03-28T14:00:00+00:00',
+    category: 'drinks',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
+    city: 'London, UK',
+    venue: 'Punch & Judy, Henrietta Street, London, UK',
+    hostedBy: 'Tom',
+    hostPhotoURL: 'https://randomuser.me/api/portraits/men/43.jpg',
+    attendees: [
+      {
+        id: 'b',
+        name: 'Tom',
+        photoURL: 'https://randomuser.me/api/portraits/women/42.jpg'
+      },
+      {
+        id: 'a',
+        name: 'Bob',
+        photoURL: 'https://randomuser.me/api/portraits/women/43.jpg'
+      },
+      {
+        id: 'd',
+        name: 'Carlet',
+        photoURL: 'https://randomuser.me/api/portraits/women/12.jpg'
+      },
+      {
+        id: 'c',
+        name: 'Jenny',
+        photoURL: 'https://randomuser.me/api/portraits/women/4.jpg'
+      }
+    ]
+  }
+]
+
+class Eventsdashboard extends Component {
+  state={
+    events:eventsFromDashboard,
+    isOpen:false
+  }
+  handleIsOpenToggle = () =>{
+    this.setState((prevState)=>({
+      isOpen:!prevState.isOpen
+    }))
+  }
+    render() {
+      const{ events,isOpen }=this.state;
+        return (
+            <Grid>
+                <Grid.Column width={10}>
+                    <EventList events={events} />
+                    </Grid.Column>
+                <Grid.Column width={6}>
+                    <Button positive content='Create Event' onClick={this.handleIsOpenToggle}/>
+                    {isOpen && <EventForm  cancelFormOpen={this.handleIsOpenToggle} />}
+                    </Grid.Column>
+            </Grid>
+        )
+    }
+}
+export default Eventsdashboard;
